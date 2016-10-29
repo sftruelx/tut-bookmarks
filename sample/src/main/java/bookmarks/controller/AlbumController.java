88ec809b8f7fe4a6f778ad2ae038d8edb89223ac
truelx <truelx@163.com>
2016-10-29 20:33:48 +0800

    sdfasdfasdfasdfa sd

--- sample/src/main/java/bookmarks/controller/AlbumController.java
+++ sample/src/main/java/bookmarks/controller/AlbumController.java
@@ -1,17 +1,32 @@
 package bookmarks.controller;
 
 
+import bookmarks.bootstraptable.Paginator;
+import bookmarks.bootstraptable.PaginatorResult;
 import bookmarks.entity.Album;
 import bookmarks.repository.AlbumRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hibernate.validator.internal.util.CollectionHelper.newArrayList;
 
 @Controller
 @RequestMapping("/album")
@@ -33,11 +48,30 @@ public class AlbumController {
         this.albumRepository = albumRepository;
     }
 
+
     @ResponseBody
     @GetMapping
-    public Iterable<Album> list() {
-        Iterable<Album> albums = this.albumRepository.findAll();
-        return albums;
+    public PaginatorResult list(Paginator paginator, Album album ) {
+        if(paginator.getLimit() <=0) paginator.setLimit(10);
+        Pageable pageable = new PageRequest(paginator.getOffset()/paginator.getLimit(), paginator.getLimit());
+        Page<Album> fields = this.albumRepository.findAll(new Specification<Album>() {
+            @Override
+            public Predicate toPredicate(Root<Album> root,
+                                         CriteriaQuery<?> q, CriteriaBuilder cb) {
+                List<Predicate> list = newArrayList();
+                Predicate albumName = null;
+                if(!StringUtils.isEmpty(album.getAlbumName())) {
+                    albumName = cb.like(root.get("albumName").as(String.class), "%"+album.getAlbumName()+"%");
+                    list.add(albumName);
+                }
+                Predicate enabled = cb.equal(root.get("enabled").as(Integer.class), 1);
+                list.add(enabled);
+                Predicate[] p = new Predicate[list.size()];
+                return cb.and(list.toArray(p));
+            }
+        },pageable);
+        PaginatorResult result = new PaginatorResult(fields.getContent(),fields.getTotalElements());
+        return result;
     }
 
     @ResponseBody
--- sample/src/main/java/bookmarks/entity/Album.java
+++ sample/src/main/java/bookmarks/entity/Album.java
@@ -1,4 +1,5 @@
 package bookmarks.entity;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -7,71 +8,88 @@ import java.util.Date;
 @Table(name = "album")
 public class Album {
 
-private Long id;
-private String albumName;
-private String author;
-private Long classifyId;
-private Date createTime;
-private String descripe;
-private String imgPath;
-private Date publishDate;
+    private Long id;
+    private String albumName;
+    private String author;
+    private Long classifyId;
+    private Date createTime;
+    private String descripe;
+    private String imgPath;
+    private Integer enabled = 1;
+    private Date publishDate;
 
     @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  public Long getId() {
-return id;
-}
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
 
-public void setId(Long id) {
-this.id = id;
-}
-  public String getAlbumName() {
-return albumName;
-}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-public void setAlbumName(String albumName) {
-this.albumName = albumName;
-}
-  public String getAuthor() {
-return author;
-}
+    public String getAlbumName() {
+        return albumName;
+    }
 
-public void setAuthor(String author) {
-this.author = author;
-}
-  public Long getClassifyId() {
-return classifyId;
-}
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
 
-public void setClassifyId(Long classifyId) {
-this.classifyId = classifyId;
-}
-  public Date getCreateTime() {
-return createTime;
-}
+    public String getAuthor() {
+        return author;
+    }
 
-public void setCreateTime(Date createTime) {
-this.createTime = createTime;
-}
-  public String getDescripe() {
-return descripe;
-}
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-public void setDescripe(String descripe) {
-this.descripe = descripe;
-}
-  public String getImgPath() {
-return imgPath;
-}
+    public Long getClassifyId() {
+        return classifyId;
+    }
 
-public void setImgPath(String imgPath) {
-this.imgPath = imgPath;
-}
-  public Date getPublishDate() {
-return publishDate;
-}
+    public void setClassifyId(Long classifyId) {
+        this.classifyId = classifyId;
+    }
 
-public void setPublishDate(Date publishDate) {
-this.publishDate = publishDate;
-}
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getDescripe() {
+        return descripe;
+    }
+
+    public void setDescripe(String descripe) {
+        this.descripe = descripe;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+
+    public Integer getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Integer enabled) {
+        this.enabled = enabled;
+    }
 }
